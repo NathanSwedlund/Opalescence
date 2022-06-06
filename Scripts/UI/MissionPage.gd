0a01fb8677c8eda9a7869bf246e459ab5bc408ee
NathanSwedlund <nathanswedlund@gmail.com>
2022-06-06 11:54:26 -0500

    Adds back and ready buttons to tutorial and mission pages

--- Scripts/UI/MissionPage.gd
+++ Scripts/UI/MissionPage.gd
@@ -38,15 +38,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(Input.is_action_just_pressed("ui_cancel")):
-		Global.ui_states[ui_name] = selected
-		get_tree().change_scene("res://Scenes/MainScenes/MainMenu.tscn")
+		back_to_main_menu()
 		
 	get_parent().find_node("Particles2D").visible = !is_shifting
 
 	if(is_shifting == false):
-		if(Input.is_action_just_pressed("ui_accept")):
-			load_scene_from_panel()
-		
 		if(Input.is_action_just_pressed("ui_left") and selected != 0):
 			shift_right = false
 			start_shifting()
@@ -96,23 +92,40 @@ func change_color(color):
 	get_parent().find_node("Particles2D").modulate = color
 	get_parent().find_node("Particles2D").find_node("Light2D").color = color
 	
-	get_parent().find_node("MissionContainerFrame").modulate = color
-	get_parent().find_node("MissionContainerFrame").modulate.r += ui_modulate_saturation_mod
-	get_parent().find_node("MissionContainerFrame").modulate.g += ui_modulate_saturation_mod
-	get_parent().find_node("MissionContainerFrame").modulate.b += ui_modulate_saturation_mod
-	get_parent().find_node("MissionContainerFrame").modulate.a = 1.0
+	var mission_container_frame  = get_parent().find_node("MissionContainerFrame")
+	mission_container_frame.modulate = color
+	mission_container_frame.modulate.r += ui_modulate_saturation_mod
+	mission_container_frame.modulate.g += ui_modulate_saturation_mod
+	mission_container_frame.modulate.b += ui_modulate_saturation_mod
+	mission_container_frame.modulate.a = 1.0
 	
-	get_parent().find_node("NextPanelButton").modulate = color
-	get_parent().find_node("NextPanelButton").modulate.r += ui_modulate_saturation_mod * -3
-	get_parent().find_node("NextPanelButton").modulate.g += ui_modulate_saturation_mod * -3
-	get_parent().find_node("NextPanelButton").modulate.b += ui_modulate_saturation_mod * -3
-	get_parent().find_node("NextPanelButton").modulate.a = 1.0
+	var next_panel_button = get_parent().find_node("Buttons").find_node("NextPanelButton")
+	next_panel_button.modulate = color
+	next_panel_button.modulate.r += ui_modulate_saturation_mod * -3
+	next_panel_button.modulate.g += ui_modulate_saturation_mod * -3
+	next_panel_button.modulate.b += ui_modulate_saturation_mod * -3
+	next_panel_button.modulate.a = 1.0
 	
-	get_parent().find_node("LastPanelButton").modulate = color
-	get_parent().find_node("LastPanelButton").modulate.r += ui_modulate_saturation_mod * -3
-	get_parent().find_node("LastPanelButton").modulate.g += ui_modulate_saturation_mod * -3
-	get_parent().find_node("LastPanelButton").modulate.b += ui_modulate_saturation_mod * -3
-	get_parent().find_node("LastPanelButton").modulate.a = 1.0
+	var last_panel_button = get_parent().find_node("Buttons").find_node("LastPanelButton")
+	last_panel_button.modulate = color
+	last_panel_button.modulate.r += ui_modulate_saturation_mod * -3
+	last_panel_button.modulate.g += ui_modulate_saturation_mod * -3
+	last_panel_button.modulate.b += ui_modulate_saturation_mod * -3
+	last_panel_button.modulate.a = 1.0
+	
+	var ready_button = get_parent().find_node("Buttons").find_node("ButtonSelectionController").find_node("ReadyButton")
+	ready_button.modulate = color
+	ready_button.modulate.r += ui_modulate_saturation_mod * -3
+	ready_button.modulate.g += ui_modulate_saturation_mod * -3
+	ready_button.modulate.b += ui_modulate_saturation_mod * -3
+	ready_button.modulate.a = 1.0
+	
+	var back_button = get_parent().find_node("Buttons").find_node("ButtonSelectionController").find_node("BackButton")
+	back_button.modulate = color
+	back_button.modulate.r += ui_modulate_saturation_mod * -3
+	back_button.modulate.g += ui_modulate_saturation_mod * -3
+	back_button.modulate.b += ui_modulate_saturation_mod * -3
+	back_button.modulate.a = 1.0
 	
 	$Pages.get_children()[selected].find_node("Description").visible = true
 	$Pages.get_children()[selected].modulate.a = 1.0
@@ -175,3 +188,12 @@ func select(p):
 	position.x += shift_dist * selected
 	change_color($Pages.get_child(selected).modulate)
 
+func back_to_main_menu():
+	Global.ui_states[ui_name] = selected
+	get_tree().change_scene("res://Scenes/MainScenes/MainMenu.tscn")
+
+func _on_ReadyButton_pressed():
+	load_scene_from_panel()
+
+func _on_BackButton_pressed():
+	back_to_main_menu()
