522c9427310a0ee6f4151179e41cd56e7d2ec7e5
nathan.swedlund@gmail.com <nathan.swedlund@gmail.com>
2022-07-23 21:10:51 -0500

    Updates laser player light

--- Godot/Scenes/MainScenes/PlayerType2.gd
+++ Godot/Scenes/MainScenes/PlayerType2.gd
@@ -2,10 +2,12 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 var player = null
+export var light_scale = 2
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if(player != null):
 		$Light2D.color = player.modulate
+		$Light2D.scale = player.find_node("OuterLight").scale*light_scale
 	else:
 		if( get_parent().player != null):
 			player = get_parent().player
